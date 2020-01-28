ebd8e03e14bcc30313c9cec0fac4d33bf4828b78
frequem <frequem@users.noreply.github.com>
2020-01-28 21:09:06 +0100

    update stringhelper to 1.0.5

---
+++ package/stringhelper/example/1.0.5/main.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stringhelper/string.h>
+
+int main(int argc, char **argv) {
+	string_t s = STRING_INITIALIZER;
+	string_copy(&s, string_cstr("ok!\n"));
+	string_write(STDOUT_FILENO, &s);
+	string_free(&s);
+    return 0;
+}
--- package/stringhelper/package.cmake
+++ package/stringhelper/package.cmake
@@ -1,7 +1,15 @@
 pacmake_include(register_package)
 pacmake_include(default_version)
 
-pacmake_set_default_version(stringhelper 1.0.4)
+pacmake_set_default_version(stringhelper 1.0.5)
+
+
+pacmake_register_package(
+	stringhelper
+	VERSION 1.0.5
+	URL "https://github.com/frequem/stringhelper/archive/v1.0.5.tar.gz"
+	URL_HASH "SHA256=68714826dd215e0407f0049a0effec4e96e005cdc4b3a29274dce52f96f30d0d"
+)
 
 pacmake_register_package(
 	stringhelper
