5c25d9113f7e0807beae05a70d1dbebebba87488
frequem <frequem@users.noreply.github.com>
2020-05-14 17:00:19 +0200

    glatter: force release

--- package/glatter/patch/preconfigure.cmake
+++ package/glatter/patch/preconfigure.cmake
@@ -1,6 +1,12 @@
 pacmake_include(textfile)
 
 function(pacmake_patch patchdir sourcedir package version)
+	file(WRITE "${sourcedir}/${package}Config.cmake"
+		"include(CMakeFindDependencyMacro)\n"
+		"find_dependency(OpenGL REQUIRED)\n"
+		"include(\"\${CMAKE_CURRENT_LIST_DIR}/${package}Targets.cmake\")\n"
+	)
+	
 	pacmake_textfile_read("${patchdir}/files/CMakeLists.txt" cmakelists)
 	string(REPLACE "@@@___VERSION___@@@" "${version}" cmakelists "${cmakelists}")
 	pacmake_textfile_write("${sourcedir}/CMakeLists.txt" ${cmakelists})
@@ -9,5 +15,10 @@ function(pacmake_patch patchdir sourcedir package version)
 	pacmake_textfile_replace("${sourcedir}/include/glatter/glatter_config.h" 
 		"#define GLATTER_HEADER_ONLY"
 		"//#define GLATTER_HEADER_ONLY"
+	)
+	#disable debug build
+	pacmake_textfile_replace("${sourcedir}/include/glatter/glatter_config.h" 
+		"#define GLATTER_LOG_ERRORS"
+		"//#define GLATTER_LOG_ERRORS"
 	)
 endfunction(pacmake_patch)
