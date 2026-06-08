987ef642ded697083edb64cd043b8cb8f7d68e15
frequem <frequem@github.com>
2026-06-08 12:16:17 +0200

    add volk

---
+++ package/volk/package.cmake
@@ -0,0 +1,5 @@
+pacmake_register_package(
+	1.4.350 FINAL
+	SOURCES URL "https://github.com/zeux/volk/archive/refs/tags/1.4.350.tar.gz" URL_HASH "SHA256=e47c6efe5294bb03729a976b385864bba5daf46ec60f0bdea11e1d1446345f9a"
+	CMAKE_ARGS "-DVOLK_INSTALL=ON"
+)
